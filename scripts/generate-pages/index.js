24804af25f9206fef56f245a800de2ef06b2249d
aegisfleet <aegisfleet@gmail.com>
2025-08-10 22:11:16 +0900

    Format: Consolidate sitemap schemaLocation attribute
    Consolidate the `xsi:schemaLocation` attribute in both generated and static sitemap files to a single line for improved readability and consistency.

--- scripts/generate-pages/index.js
+++ scripts/generate-pages/index.js
@@ -98,12 +98,16 @@ class PageGenerator {
     generateSitemap(archives) {
         const baseUrl = 'https://aegisfleet.github.io/live-stream-summarizer';
         let sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n';
-        sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n';
-        sitemap += '  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n';
-        sitemap += '  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9\n';
-        sitemap += '  http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n';
+        sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n';
         // トップページ
-        sitemap += `  <url>\n    <loc>${baseUrl}/</loc>\n    <changefreq>daily</changefreq>\n    <priority>1.0</priority>\n  </url>\n`;
+        const now = new Date().toISOString();
+        sitemap += `  <url>
+    <loc>${baseUrl}/</loc>
+    <lastmod>${now}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+`;
         
         // 個別ページ
         archives.forEach(archive => {

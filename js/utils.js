c7dd72547b860a0182090e3ead6d368ced73d29c
aegisfleet <aegisfleet@users.noreply.github.com>
2025-08-12 15:57:56 +0000

    deploy: 23e30e85d99cf43adb98dfd194bfb99532629ca4

--- js/utils.js
+++ js/utils.js
@@ -3,20 +3,32 @@ export function timestampToSeconds(timestamp) {
     return parts.reduce((total, part, index) => total + part * Math.pow(60, index), 0);
 }
 
-export function formatDuration(totalSeconds) {
+export function formatDuration(totalSeconds, lang = 'ja') {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
 
     let durationStr = "";
-    if (hours > 0) {
-        durationStr += `${hours}時間`;
-    }
-    if (minutes > 0) {
-        durationStr += `${minutes}分`;
-    }
-    if (seconds > 0 || durationStr === "") {
-        durationStr += `${seconds}秒`;
+    if (lang === 'en') {
+        if (hours > 0) {
+            durationStr += `${hours}h `;
+        }
+        if (minutes > 0) {
+            durationStr += `${minutes}m `;
+        }
+        if (seconds > 0 || durationStr === "") {
+            durationStr += `${seconds}s`;
+        }
+    } else { // Default to Japanese
+        if (hours > 0) {
+            durationStr += `${hours}時間`;
+        }
+        if (minutes > 0) {
+            durationStr += `${minutes}分`;
+        }
+        if (seconds > 0 || durationStr === "") {
+            durationStr += `${seconds}秒`;
+        }
     }
     return durationStr.trim();
 }

b4f7ddafb70e7c7368805888f0a8989f399fea0c
aegisfleet <aegisfleet@users.noreply.github.com>
2025-09-12 23:16:41 +0000

    deploy: 39969886f9d809b67db1ea14d8a8e34bb1f3b09a

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hololive-summary-cache-v1-1757710285050';
+const CACHE_NAME = 'hololive-summary-cache-v1-1757718981618';
 const SITE_URL = 'https://aegisfleet.github.io/live-stream-summarizer/';
 const ASSETS_TO_CACHE = [
   './',
